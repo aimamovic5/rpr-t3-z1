3cb751ca29f06f96473491b5aeeb20e3af183b32
aimamovic5 <44173127+aimamovic5@users.noreply.github.com>
2018-10-30 14:04:34 +0100

    dodao gettere i settere klase MobilniBroj

--- src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -4,6 +4,22 @@ public class MobilniBroj extends TelefonskiBroj {
     private String Broj;
     private int MobilnaMreza;
 
+    public String getBroj() {
+        return Broj;
+    }
+
+    public void setBroj(String broj) {
+        Broj = broj;
+    }
+
+    public int getMobilnaMreza() {
+        return MobilnaMreza;
+    }
+
+    public void setMobilnaMreza(int mobilnaMreza) {
+        MobilnaMreza = mobilnaMreza;
+    }
+
     @Override
     public String ispisi() {
         return null;
