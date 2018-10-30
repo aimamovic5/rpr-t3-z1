53c5c9024aa46b6d817ebc1de0f4cb469e0904eb
aimamovic5 <44173127+aimamovic5@users.noreply.github.com>
2018-10-30 15:20:57 +0100

    dodao atribute, gettere i settere

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MedunarodniBroj.java
@@ -0,0 +1,22 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MedunarodniBroj {
+    private String Drzava;
+    private String Broj;
+
+    public String getDrzava() {
+        return Drzava;
+    }
+
+    public void setDrzava(String drzava) {
+        Drzava = drzava;
+    }
+
+    public String getBroj() {
+        return Broj;
+    }
+
+    public void setBroj(String broj) {
+        Broj = broj;
+    }
+}
