2f0cc1c6557f097d983cde9c9e6f20af67a35ad7
aimamovic5 <44173127+aimamovic5@users.noreply.github.com>
2018-10-30 14:04:08 +0100

    dodao klasu MobilniBroj sa atributima

---
+++ src/ba/unsa/etf/rpr/tutorijal03/MobilniBroj.java
@@ -0,0 +1,16 @@
+package ba.unsa.etf.rpr.tutorijal03;
+
+public class MobilniBroj extends TelefonskiBroj {
+    private String Broj;
+    private int MobilnaMreza;
+
+    @Override
+    public String ispisi() {
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
