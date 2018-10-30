d8e4075c0483ede7e6715333f169a48692fcc266
aimamovic5 <44173127+aimamovic5@users.noreply.github.com>
2018-10-30 15:50:55 +0100

    implentirao metode dajBroj i dajIme

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -1,9 +1,23 @@
 package ba.unsa.etf.rpr.tutorijal03;
 import java.util.HashMap;
+import java.util.Set;
+
 
 public class Imenik {
     private HashMap<String, TelefonskiBroj> imenik = new HashMap<String, TelefonskiBroj>();
     public void dodaj(String ime, TelefonskiBroj broj) {
         this.imenik.put(ime, broj);
+    }
+    public String dajBroj(String ime) {
+        var rezultat = (imenik.get(ime)).ispisi();
+        return rezultat;
+    }
+    public String dajIme(TelefonskiBroj broj) {
+        for (String s : imenik.keySet()) {
+            if (imenik.get(s).equals(broj)) {
+                return s;
+            }
+        }
+    return null;
     }
 }
