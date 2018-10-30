7c5fa3c58167d96dda10fa8e2959c379c81d37d0
aimamovic5 <44173127+aimamovic5@users.noreply.github.com>
2018-10-30 15:31:45 +0100

    implentirao metodu dodaj

--- src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
+++ src/ba/unsa/etf/rpr/tutorijal03/Imenik.java
@@ -3,5 +3,7 @@ import java.util.HashMap;
 
 public class Imenik {
     private HashMap<String, TelefonskiBroj> imenik = new HashMap<String, TelefonskiBroj>();
-
+    public void dodaj(String ime, TelefonskiBroj broj) {
+        this.imenik.put(ime, broj);
+    }
 }
